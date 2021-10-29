0af49bad93c3b0642e83747a234b9b9d2cdef699
Mark-Inholland <mark.dehaan@inholland.nl>
2021-10-29 11:19:44 +0200

    cleaned up controller code

--- app/public/controllers/articlecontroller.php
+++ app/public/controllers/articlecontroller.php
@@ -4,17 +4,22 @@ require __DIR__ . '/../services/articleservice.php';
 
 class ArticleController extends Controller {
 
+    private $articleService; 
+
+    // initialize services
+    function __construct() {
+        $this->articleService = new ArticleService();
+    }
+
     // router maps this to /article and /article/index automatically
     public function index() {
 
-        // retrieve data
-        $service = new ArticleService();
-        $articles = $service->getAll();
+        // retrieve data 
+        $articles = $this->articleService->getAll();
         
-        // show view, first param = view name, second param = accessible as $model in the view
-        // displayView maps this to index.php file in /views/article/ automatically
-
-        $this->displayView("index", $articles);
+        // show view, param = accessible as $model in the view
+        // displayView maps this to /views/article/index.php automatically
+        $this->displayView($articles);
     }
 }
 ?>
--- app/public/controllers/controller.php
+++ app/public/controllers/controller.php
@@ -1,9 +1,10 @@
 <?php
 class Controller {
     
-    function displayView($name, $model) {        
+    function displayView($model) {        
         $directory = substr(get_class($this), 0, -10);
-        require __DIR__ . "/../views/$directory/$name.php";
+        $view = debug_backtrace()[1]['function'];
+        require __DIR__ . "/../views/$directory/$view.php";
     }
 
 }
